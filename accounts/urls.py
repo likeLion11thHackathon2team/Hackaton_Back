3bbaa19ab000a0d617c2db815602dc06a7f509fe
zi00iz <zii0qkr@gmail.com>
2023-08-18 21:54:56 +0900

    profile 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 from rest_framework import urls
 
-#회원가입
 
 urlpatterns =[
     path('signup/', views.UserCreate.as_view()),                     
