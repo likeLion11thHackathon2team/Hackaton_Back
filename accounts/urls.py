3418919ae6add4e374f6029a4be2237175bfb6d1
sm4640 <s1m0611@naver.com>
2023-09-08 09:20:57 +0900

    프로필세팅

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns =[
     path('signup/', views.UserCreate.as_view()),                     
     path('api-auth/', include('rest_framework.urls')),
     path('login/', views.UserLogin.as_view()),
+    path('profile/<int:pk>/', views.UserProfile.as_view()),
 ]
